19e8713640c9aaa398251af8a074c455f98e6861
Nikhil Kumar <kumarnikhil48578@gmail.com>
2024-05-06 01:38:37 +0530

    Add react-router-dom, react-icons, and @giphy/js-fetch-api dependencies
    Update package.json, App.jsx, and index.css to include new dependencies and set up routing for the

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,48 @@
-import { useState } from 'react'
+import {RouterProvider, createBrowserRouter} from "react-router-dom";
+import "./index.css";
+import AppLayout from "./Layouts/AppLayout.jsx";
+import Home from "./pages/Home.jsx";
+import GifProvider from "./context/context.jsx";
+import SearchPage from "./pages/Search.jsx";
+import Category from "./pages/Categories.jsx";
+import GifPage from "./pages/SingleGif.jsx";
+import Favorites from "./pages/Favorites.jsx";
 
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
 
-function App() {
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/:type/:slug",
+        element: <GifPage />,
+      },
+      {
+        path: "/:category",
+        element: <Category />,
+      },
+      {
+        path: "/search/:query",
+        element: <SearchPage />,
+      },
+      {
+        path: "/favorites",
+        element: <Favorites />,
+      },
+    ],
+  },
+]);
 
-
+const App = () => {
   return (
-    <>
-    
-    </>
-  )
-}
+    <GifProvider>
+      <RouterProvider router={router} />
+    </GifProvider>
+  );
+};
 
-export default App
+export default App;
---
+++ src/context/context.jsx
@@ -0,0 +1,22 @@
+import { GiphyFetch } from "@giphy/js-fetch-api";
+import {createContext, useContext, useEffect, useState} from "react";
+
+const GifContext = createContext()
+
+const GifProvider = ({ children }) => {
+
+    const [gifs, setGifs] = useState([]);
+    const [filter, setFilter] = useState("gifs");
+    const [favorites, setFavorites] = useState([]);
+
+
+    const gf = new GiphyFetch(import.meta.env.VITE_GIPHY_KEY)
+
+    return <GifContext.Provider value={{gf, gifs, setGifs, filter, setFilter, favorites}}>{ children}</GifContext.Provider>
+}
+
+export const GifState = () => {
+    return useContext(GifContext)
+}
+
+export default GifProvider
